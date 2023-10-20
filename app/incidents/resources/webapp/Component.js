bf3be2dfbff67c859f214e749c4436ab90c4d2c5
I584814 <diya.dhandapani.shanmugam@sap.com>
2023-10-20 17:32:12 +0530

    changed business solution

--- app/incidents/resources/webapp/Component.js
+++ app/incidents/resources/webapp/Component.js
@@ -3,7 +3,7 @@ sap.ui.define(
     function (Component) {
         "use strict";
 
-        return Component.extend("ns.incidents.Component", {
+        return Component.extend("incidents.Component", {
             metadata: {
                 manifest: "json"
             }
--- app/incidents/resources/webapp/test/integration/pages/IncidentsList.js
+++ app/incidents/resources/webapp/test/integration/pages/IncidentsList.js
@@ -8,7 +8,7 @@ sap.ui.define(['sap/fe/test/ListReport'], function(ListReport) {
 
     return new ListReport(
         {
-            appId: 'ns.incidents',
+            appId: 'incidents',
             componentId: 'IncidentsList',
             entitySet: 'Incidents'
         },
